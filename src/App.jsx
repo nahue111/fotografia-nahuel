578868bc6583d811469cff0fc98ef4b99f865601
Nahuel Lopez <nahuel.lopez.11.mateo@gmail.com>
2026-05-17 22:36:22 -0300

    Instant scroll on route change, restore smooth after

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,18 @@ import NuestroEquipo from './pages/NuestroEquipo'
 function ScrollToTop() {
   const { pathname, hash } = useLocation()
   useEffect(() => {
+    document.documentElement.style.scrollBehavior = 'auto'
     if (!hash) {
       window.scrollTo(0, 0)
     } else {
       setTimeout(() => {
         const el = document.getElementById(hash.slice(1))
-        if (el) el.scrollIntoView({ behavior: 'smooth' })
+        if (el) el.scrollIntoView()
       }, 100)
     }
+    setTimeout(() => {
+      document.documentElement.style.scrollBehavior = ''
+    }, 200)
   }, [pathname, hash])
   return null
 }
