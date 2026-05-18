4631aa6fef0afc051af3006a5b7b5185a71c5a47
Nahuel Lopez <nahuel.lopez.11.mateo@gmail.com>
2026-05-17 21:47:31 -0300

    Scroll to top on page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Navigation from './components/Navigation'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import NuestroEquipo from './pages/NuestroEquipo'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
